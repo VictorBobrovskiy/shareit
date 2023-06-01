8e93d9de8952a1e660eb7eaf898b7819d790ebf8
porrero <porrero@yandex.ru>
2023-06-01 10:36:42 -0300

    feat: bookings passed

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -9,12 +9,7 @@ import ru.practicum.shareit.user.User;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 
 @Data
